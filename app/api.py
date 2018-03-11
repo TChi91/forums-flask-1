568d91604159886013b07071e9589dba53efda47
Unknown <majdabboud81@gmail.com>
2018-03-12 01:16:00 +0200

    Handling exceptions well

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, render_template, abort
+from flask import jsonify, request, abort
 from app import models
 from app import app, member_store, post_store
 
@@ -12,44 +12,55 @@ def topic_get_all():
 @app.route("/api/topic/add", methods=["POST"])
 def topic_create():
     request_data = request.get_json()
-    new_post = models.Post(request_data["title"], request_data["content"])
-    post_store.add(new_post)
-    return jsonify(new_post.__dict__())
+    try:
+        new_post = models.Post(request_data["title"], request_data["content"])
+        post_store.add(new_post)
+        result = jsonify(new_post.__dict__())
+    except KeyError:
+        result = abort(400, f"Couldn't parse the request data !")
+
+    return result
 
 
 @app.route("/api/topic/view/<int:id>", methods=["VIEW"])
 def topicView(id):
     post = post_store.get_by_id(id)
-    if post is None:
-        abort(404)
+    try:
+        result = jsonify(post.__dict__())
+    except AttributeError:
+        result = abort(404, f"topic with id: {id} doesn't exist")
 
-    return jsonify(post.__dict__())
+    return result
 
 
 @app.route("/api/topic/update/<int:id>", methods=["PUT"])
 def topicUpdate(id):
     request_data = request.get_json()
     post = post_store.get_by_id(id)
-    if post is None:
-        abort(404)
+    try:
+        post.title = request_data["title"]
+        post.content = request_data["content"]
+        post_store.update(post)
+        result = jsonify(post.__dict__())
+    except AttributeError:
+        result = abort(404, f"topic with id: {id} doesn't exist")
+    except KeyError:
+        result = abort(400, f"Couldn't parse the request data !")
 
-    post.title = request_data["title"]
-    post.content = request_data["content"]
-    post_store.update(post)
-
-    return jsonify(post.__dict__())
+    return result
 
 
 @app.route("/api/topic/delete/<int:id>", methods=["DELETE"])
 def topicDelete(id):
     try:
-        post_store.delete(id)
+        result = post_store.delete(id)
+        result = jsonify(result.__dict__())
     except ValueError:
-        abort(404)
+        result = abort(404, f"topic with id: {id} doesn't exist")
 
-    return topic_get_all()
+    return result
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify(message = error.description)
