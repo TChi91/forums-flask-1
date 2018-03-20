62295092115c18b7693667ce26d4f9a053f34965
Unknown <majdabboud81@gmail.com>
2018-03-20 15:39:57 +0200

    editing files to use DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
-from app import store, models, dummy_data
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 
+folder_path = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+from app import store, dummy_data
 
 member_store = store.MembersStore()
 post_store = store.PostsStore()
-dummy_data.seed_stores(member_store, post_store)
-
+#dummy_data.seed_stores(member_store, post_store)
 
 from app import views
+from app import api
