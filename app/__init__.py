2aa752d2271a1602f9def665abb718f4dc64d31f
Majd Abboud <majdabboud81@gmail.com>
2018-03-22 15:21:40 +0200

    St.Anger

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ import os
 app = Flask(__name__)
 
 folder_path = os.path.abspath(os.path.dirname(__file__))
-#app.config["SQLALCHEMY_DATABASE_URI"] = """sqlite:///{0}""".format(os.path.join(folder_path, "my_database.db"))
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+app.config["SQLALCHEMY_DATABASE_URI"] = """sqlite:///{0}""".format(os.path.join(folder_path, "my_database.db"))
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
@@ -15,7 +15,7 @@ from app import store, dummy_data
 
 member_store = store.MembersStore()
 post_store = store.PostsStore()
-#dummy_data.seed_stores(member_store, post_store)
+dummy_data.seed_stores(member_store, post_store)
 
 from app import views
 from app import api
