c6e535907a2d5d2e95f099b77e555301a6cc2dc5
Majd Abboud <majdabboud81@gmail.com>
2018-03-22 19:41:17 +0200

    init edit

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, psycopg2
 
 
 app = Flask(__name__)
 
 folder_path = os.path.abspath(os.path.dirname(__file__))
 #app.config["SQLALCHEMY_DATABASE_URI"] = """sqlite:///{0}""".format(os.path.join(folder_path, "my_database.db"))
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://hekylxbdgwehkd:352fb94ac16ea338bb2a13a9570185bf058b030e7321a4036a1a861464e01384@ec2-54-197-250-121.compute-1.amazonaws.com:5432/d6v4kqaekjsfj'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
